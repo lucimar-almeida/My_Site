e38336266e1b07ae9853531c98a82e8bc17eae29
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-10-09 19:02:20 -0400

    Pagina pessoal Python

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Criar a primeira pagina
+# route -> lucimaralmeida.com/contatos
+# função -> o quer exibir naquela pagina
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
+# Colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# criar o arquivo requirements
